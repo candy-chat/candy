2860b616fdec71ee948f0b0c55b49a9d1a16062d
Patrick Forget <patforg@geekpad.ca>
2011-10-26 00:22:22 -0400

    forgot to add core/message.js to Makefile

--- candy.bundle.js
+++ candy.bundle.js
@@ -2049,7 +2049,56 @@ Candy.Core.Event = (function(self, Strophe, $, observable) {
 
 	return self;
 }(Candy.Core.Event || {}, Strophe, jQuery, Candy.Util.Observable));
-/** File: event.js
+/** File: message.js
+ * Candy - Chats are not dead yet.
+ *
+ * Authors:
+ *   - Patrick Stadler <patrick.stadler@amiadogroup.com>
+ *   - Michael Weibel <michael.weibel@amiadogroup.com>
+ *   - Patrick Forget <patforg@geekpad.ca>
+ *
+ * Copyright:
+ *   (c) 2011 Amiado Group AG. All rights reserved.
+ */
+
+/** Class: Candy.Core.Message
+ * Chat messages 
+ *
+ * Parameters:
+ *   (String) text - the message text
+ */
+Candy.Core.Message = function(text, type) {
+	/** String: _text
+	 * contains message text
+	 */
+	this._text = text;
+        
+        /** String: _type
+	 * type of message
+	 */
+	this._type = type;
+
+	/** Function: setText
+	 * Set the message's text
+	 *
+	 * Parameters:
+	 *   (String) text - message text to set
+	 */
+	this.setText = function(text) {
+		this._text = text;
+	};
+
+	/** Function: getText
+	 * Get message text
+	 *
+	 * Returns:
+	 *   (String) - the message's text
+	 */
+	this.getText = function() {
+		return this._text;
+	};
+
+};/** File: event.js
  * Candy - Chats are not dead yet.
  *
  * Authors:
