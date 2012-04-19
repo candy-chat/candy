89b6455741d27a3f11ab51fb392dc0fa51d88e4d
Michael Weibel <michael.weibel@gmail.com>
2012-04-19 09:35:27 +0200

    Track clicks on demo button - we'd like to know how many visitors actually take a look at the demo and for how long

--- public/application.js
+++ public/application.js
@@ -10,12 +10,18 @@ $(document).ready(function() {
 			showcase.removeClass('demomode');
 			$(this).text('Try the Demo').removeClass('demomode');
 			$('#demoarrow').stop(true, true).fadeOut();
+			if (_gaq !== undefined) {
+				_gaq.push(['_trackEvent', 'Demo', 'Stop']);
+			}
 		} else {
 			$('html,body').stop().animate({'scrollTop': Math.floor(showcase.offset().top) + 1});
 			showcase.html('<iframe src="http://dev.openflex.net/candy" scrolling="no" frameborder="0"></iframe>');
 			showcase.addClass('demomode');
 			$(this).text('ONLINE').addClass('demomode');
 			$('#demoarrow').stop(true, true).fadeIn();
+			if (_gaq !== undefined) {
+				_gaq.push(['_trackEvent', 'Demo', 'Start']);
+			}
 		}
 	});	
 });
