fbb72e28753a932c9eb1bc154443220b55073c41
Francesco Barresi <francesco@francesco.(none)>
2012-05-17 12:07:47 +0200

    Added italian translation. Copied from github.com/nephila/candy and made come corrections.

--- src/view/translation.js
+++ src/view/translation.js
@@ -471,5 +471,70 @@ Candy.View.Translation = {
 		'errorMaxOccupantsReached'  : '"%s"の部屋に入ることができません: 参加者の数はすでに上限に達しました。',
 
 		'antiSpamMessage'   : 'スパムなどの行為はやめてください。あなたは一時的にブロックされました。'
-	}
+	},
+    'it' : {
+        'status': 'Stato: %s',
+        'statusConnecting': 'Connessione...',
+        'statusConnected' : 'Connesso',
+        'statusDisconnecting': 'Disconnessione...',
+        'statusDisconnected' : 'Disconnesso',
+        'statusAuthfail': 'Autenticazione fallita',
+
+        'roomSubject'  : 'Oggetto:',
+        'messageSubmit': 'Invia',
+
+        'labelUsername': 'Nome utente:',
+        'labelPassword': 'Password:',
+        'loginSubmit'  : 'Login',
+        'loginInvalid'  : 'JID non valido',
+
+        'reason'                : 'Ragione:',
+        'subject'               : 'Oggetto:',
+        'reasonWas'             : 'La ragione è: %s.',
+        'kickActionLabel'       : 'Espelli',
+        'youHaveBeenKickedBy'   : 'Sei stato espulso da %2$s da %1$s',
+        'youHaveBeenKicked'     : 'Sei stato espulso da %s',
+        'banActionLabel'        : 'Escluso',
+        'youHaveBeenBannedBy'   : 'Sei stato escluso da %1$s da %2$s',
+        'youHaveBeenBanned'     : 'Sei stato escluso da %s',
+
+        'privateActionLabel' : 'Stanza privata',
+        'ignoreActionLabel'  : 'Ignora',
+        'unignoreActionLabel' : 'Non ignorare',
+
+        'setSubjectActionLabel': 'Cambia oggetto',
+
+        'administratorMessageSubject' : 'Amministratore',
+
+        'userJoinedRoom'           : '%s si è unito alla stanza.',
+        'userLeftRoom'             : '%s ha lasciato la stanza.',
+        'userHasBeenKickedFromRoom': '%s è stato espulso dalla stanza.',
+        'userHasBeenBannedFromRoom': '%s è stato escluso dalla stanza.',
+
+        'presenceUnknownWarningSubject': 'Notare:',
+        'presenceUnknownWarning'       : 'Questo utente potrebbe essere offline. Non possiamo tracciare la sua presenza.',
+
+        'dateFormat': 'dd.mm.yyyy',
+        'timeFormat': 'HH:MM:ss',
+
+        'tooltipRole'           : 'Moderatore',
+        'tooltipIgnored'        : 'Stai ignorando questo utente',
+        'tooltipEmoticons'      : 'Emoticons',
+        'tooltipSound'          : 'Riproduci un suono quando arrivano messaggi privati',
+        'tooltipAutoscroll'     : 'Autoscroll',
+        'tooltipStatusmessage'  : 'Mostra messaggi di stato',
+        'tooltipAdministration' : 'Amministrazione stanza',
+        'tooltipUsercount'      : 'Partecipanti alla stanza',
+
+        'enterRoomPassword' : 'La stanza "%s" è protetta da password.',
+        'enterRoomPasswordSubmit' : 'Unisciti alla stanza',
+        'passwordEnteredInvalid' : 'Password non valida per la stanza "%s".',
+
+        'nicknameConflict': 'Nome utente già in uso. Scegline un altro.',
+
+        'errorMembersOnly': 'Non puoi unirti alla stanza "%s": Permessi insufficienti.',
+        'errorMaxOccupantsReached': 'Non puoi unirti alla stanza "%s": Troppi participanti.',
+
+        'antiSpamMessage' : 'Per favore non scrivere messaggi pubblicitari. Sei stato bloccato per un po\' di tempo.'
+    }
 };
