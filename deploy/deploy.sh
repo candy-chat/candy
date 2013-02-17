68fb99add9186e121e0a9f1311d1ffbf1dbfb335
Drew Harry <drew.harry@gmail.com>
2013-02-17 13:22:12 -0500

    Deploy script updated.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,9 +5,7 @@
 
 scp -i ~/Documents/multitude/infrastructure/ec2/multitude.pem index.html ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
 
-scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ../src ../libs ../*.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
-
-scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ../src ../libs ../*.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
 
 scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../src ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
 
