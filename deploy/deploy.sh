d11bbe0e78d4aac5f9f6784e2127685fb9255173
Drew Harry <drew.harry@gmail.com>
2013-02-17 12:52:12 -0500

    Simple and dirty deploy script added.

---
+++ deploy/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this is all super hyper custom for me and my machine, buttt
+# gotta have a nice way to do it.
+
+scp -i ~/Documents/multitude/infrastructure/ec2/multitude.pem index.html ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ../src ../libs ../*.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ../src ../libs ../*.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../src ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../libs ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
+scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../*.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
