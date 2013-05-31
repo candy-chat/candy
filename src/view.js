8c9370aca8de978429b805ca2657161a1f585a67
Michael Weibel <michael.weibel@gmail.com>
2013-05-31 21:29:09 +0200

    Fix indenting

--- src/view.js
+++ src/view.js
@@ -57,14 +57,14 @@ Candy.View = (function(self, $) {
 		 * Register observers. Candy core will now notify the View on changes.
 		 */
 		_registerObservers = function() {
-		    $(Candy.Core.Event).on('candy:core.chat.connection', self.Observer.Chat.Connection);
-		    $(Candy.Core.Event).on('candy:core.chat.message', self.Observer.Chat.Message);
-		    $(Candy.Core.Event).on('candy:core.login', self.Observer.Login);
-		    $(Candy.Core.Event).on('candy:core.presence', self.Observer.Presence.update);
+			$(Candy.Core.Event).on('candy:core.chat.connection', self.Observer.Chat.Connection);
+			$(Candy.Core.Event).on('candy:core.chat.message', self.Observer.Chat.Message);
+			$(Candy.Core.Event).on('candy:core.login', self.Observer.Login);
+			$(Candy.Core.Event).on('candy:core.presence', self.Observer.Presence.update);
 			$(Candy.Core.Event).on('candy:core.presence.leave', self.Observer.Presence.update);
-		    $(Candy.Core.Event).on('candy:core.presence.room', self.Observer.Presence.update);
-		    $(Candy.Core.Event).on('candy:core.presence.error', self.Observer.PresenceError);
-		    $(Candy.Core.Event).on('candy:core.message', self.Observer.Message);
+			$(Candy.Core.Event).on('candy:core.presence.room', self.Observer.Presence.update);
+			$(Candy.Core.Event).on('candy:core.presence.error', self.Observer.PresenceError);
+			$(Candy.Core.Event).on('candy:core.message', self.Observer.Message);
 		},
 
 		/** PrivateFunction: _registerWindowHandlers
