e444c7294f1b45a73da26e308c3a65143f6cb0ad
Michael Weibel <michael.weibel@gmail.com>
2013-12-17 18:30:01 +0100

    Add portuguese translation
    Close #143

--- src/view/translation.js
+++ src/view/translation.js
@@ -665,6 +665,71 @@ Candy.View.Translation = {
 
 		'antiSpamMessage' : 'Por favor, não envie spam. Você foi bloqueado temporariamente.'
 	},
+	'pt_br' : {
+		'status': 'Estado: %s',
+		'statusConnecting': 'Conectando...',
+		'statusConnected' : 'Conectado',
+		'statusDisconnecting': 'Desconectando...',
+		'statusDisconnected' : 'Desconectado',
+		'statusAuthfail': 'Autenticação falhou',
+
+		'roomSubject' : 'Assunto:',
+		'messageSubmit': 'Enviar',
+
+		'labelUsername': 'Usuário:',
+		'labelPassword': 'Senha:',
+		'loginSubmit' : 'Entrar',
+		'loginInvalid' : 'JID inválido',
+
+		'reason'                                : 'Motivo:',
+		'subject'                                : 'Assunto:',
+		'reasonWas'                                : 'Motivo foi: %s.',
+		'kickActionLabel'                : 'Derrubar',
+		'youHaveBeenKickedBy' : 'Você foi derrubado de %2$s por %1$s',
+		'youHaveBeenKicked' : 'Você foi derrubado de %s',
+		'banActionLabel'                : 'Banir',
+		'youHaveBeenBannedBy' : 'Você foi banido de %1$s por %2$s',
+		'youHaveBeenBanned' : 'Você foi banido de %s',
+
+		'privateActionLabel' : 'Conversa privada',
+		'ignoreActionLabel' : 'Ignorar',
+		'unignoreActionLabel' : 'Não ignorar',
+
+		'setSubjectActionLabel': 'Mudar Assunto',
+
+		'administratorMessageSubject' : 'Administrador',
+
+		'userJoinedRoom' : '%s entrou na sala.',
+		'userLeftRoom' : '%s saiu da sala.',
+		'userHasBeenKickedFromRoom': '%s foi derrubado da sala.',
+		'userHasBeenBannedFromRoom': '%s foi banido da sala.',
+
+		'presenceUnknownWarningSubject': 'Aviso:',
+		'presenceUnknownWarning' : 'Este usuário pode estar desconectado.. Não conseguimos rastrear sua presença..',
+
+		'dateFormat': 'dd.mm.yyyy',
+		'timeFormat': 'HH:MM:ss',
+
+		'tooltipRole'                        : 'Moderador',
+		'tooltipIgnored'                : 'Você ignora este usuário',
+		'tooltipEmoticons'                : 'Emoticons',
+		'tooltipSound'                        : 'Tocar som para novas mensagens privadas',
+		'tooltipAutoscroll'                : 'Auto-rolagem',
+		'tooltipStatusmessage'        : 'Exibir mensagens de estados',
+		'tooltipAdministration'        : 'Administração de Sala',
+		'tooltipUsercount'                : 'Participantes da Sala',
+
+		'enterRoomPassword' : 'Sala "%s" é protegida por senha.',
+		'enterRoomPasswordSubmit' : 'Entrar na sala',
+		'passwordEnteredInvalid' : 'Senha inváida para sala "%s".',
+
+		'nicknameConflict': 'Nome de usuário já em uso. Por favor escolha outro.',
+
+		'errorMembersOnly': 'Você não pode entrar na sala "%s": privilégios insuficientes.',
+		'errorMaxOccupantsReached': 'Você não pode entrar na sala "%s": máximo de participantes atingido.',
+
+		'antiSpamMessage' : 'Por favor, não faça spam. Você foi bloqueado temporariamente.'
+	},
 	'ru' : {
 		'status': 'Статус: %s',
 		'statusConnecting': 'Подключение...',
