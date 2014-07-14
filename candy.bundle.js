0c25dad54862e85d320aa9749d42b0ced3d08b57
Michael Weibel <michael.weibel@gmail.com>
2014-07-14 17:58:20 +0200

    v1.7.1

--- candy.bundle.js
+++ candy.bundle.js
@@ -29,7 +29,7 @@ var Candy = function(self, $) {
 	 */
     self.about = {
         name: "Candy",
-        version: "1.7.1-dev"
+        version: "1.7.1"
     };
     /** Function: init
 	 * Init view & core
--- src/candy.js
+++ src/candy.js
@@ -30,7 +30,7 @@ var Candy = (function(self, $) {
 	 */
 	self.about = {
 		name: 'Candy',
-		version: '1.7.1-dev'
+		version: '1.7.1'
 	};
 
 	/** Function: init
