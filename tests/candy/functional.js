464923d81a38796341f2ea895516524f980508da
Ben Langfeld <ben@langfeld.me>
2014-07-28 20:03:17 -0300

    Use correct URL for functional tests

--- tests/candy/functional.js
+++ tests/candy/functional.js
@@ -1,12 +1,20 @@
 /*global define */
 
+var exampleUrl;
+
+if (process.env.CANDY_VAGRANT) {
+	exampleUrl = 'http://localhost:80/';
+} else {
+	exampleUrl = 'http://localhost:8080/';
+}
+
 define([
 	'intern!bdd',
 	'intern/chai!expect'
 ], function (bdd, expect) {
 	bdd.describe('Basic chat operation', function () {
 		bdd.it('should load the correct page', function () {
-			return this.remote.get('http://localhost:8080')
+			return this.remote.get(exampleUrl)
 				.getPageTitle()
 				.then(function (title) {
 					expect(title).to.equal('Candy - Chats are not dead yet');
