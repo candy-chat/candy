155c83dc476ae8c468d1fe90c37ac80dd9888186
Michael Weibel <michael.weibel@gmail.com>
2014-07-31 04:41:57 +0200

    Skip tests in the nice way
    Related: d48160
    cc @benlangfeld

--- tests/candy/unit/util.js
+++ tests/candy/unit/util.js
@@ -39,8 +39,8 @@ define([
 			bdd.it('should escape JIDs per XEP-0106', function () {
 				expect(Candy.Util.escapeJid("<foo>@example.com")).to.equal("\\3cfoo\\3e@example.com");
 			});
-			bdd.it('should escape JIDs per XEP-0106', function () {
-				return true; // Skip this for now. See https://github.com/theintern/intern/pull/195
+			// Skip this for now. See https://github.com/theintern/intern/pull/195
+			bdd.skip('should escape JIDs per XEP-0106', function () {
 				expect(Candy.Util.escapeJid("user@host@example.com")).to.equal("user\\40host@example.com");
 			});
 			bdd.it('should escape JIDs per XEP-0106', function () {
@@ -160,8 +160,8 @@ define([
 					expect(jid.domain).to.equal("example.com");
 					expect(jid.resource).to.equal(undefined);
 				});
-				bdd.it('should parse JIDs per RFC6122', function () {
-					return true; // Skip this for now. See https://github.com/theintern/intern/pull/195
+				// Skip this for now. See https://github.com/theintern/intern/pull/195
+				bdd.skip('should parse JIDs per RFC6122', function () {
 					var jid = Candy.Util.Parser.jid("user@host@example.com");
 					expect(jid.node).to.equal("user@host");
 					expect(jid.domain).to.equal("example.com");
