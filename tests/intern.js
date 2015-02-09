3ae79917e7152ca7ccf105d617003f8819e5f118
Ben Langfeld <ben@langfeld.me>
2015-02-09 14:06:59 -0200

    Tests are currently failing on IE9

--- tests/intern.js
+++ tests/intern.js
@@ -26,7 +26,6 @@ define({
 	environments: [
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
 		{ browserName: 'firefox', version: '30', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
 		{ browserName: 'chrome', version: '31', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
 		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
